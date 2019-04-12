4f61b0968e507fd1a999c22f084198059465c4ff
Jonathan Luk <jluk87@gmail.com>
2019-04-12 22:30:02 +0000

    Automatically backed up by Learn

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,2 +1,10 @@
 class PatientsController < ApplicationController
+
+  def index
+    @appointments = Appointment.all
+  end
+
+  def show
+    @appointment = Appointment.find_by(id: params[:id])
+  end
 end
